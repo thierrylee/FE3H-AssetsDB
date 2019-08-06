55184f668e0c4982ca797b1915d22bf7480e8c32
Thierry LEE <tlee@octo.com>
2019-08-06 23:22:02 +0200

    Setting up SQLDelight

--- core/src/main/java/me/thierrylee/fe3hdb/core/db/Db.kt
+++ core/src/main/java/me/thierrylee/fe3hdb/core/db/Db.kt
@@ -1,2 +1,11 @@
 package me.thierrylee.fe3hdb.core.db
 
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+
+class Db{
+
+    fun test(){
+        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+    }
+
+}
